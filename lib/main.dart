0f8a1b598ef06f70acbd8bcd3b1c71b3977ea4cf
ilhamlii21 <ilhamlii0521@gmail.com>
2026-04-27 10:37:03 +0700

    feat: initialize Supabase and set up main entry point with ComparisonScreen as root

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Smart Product Comparison',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
